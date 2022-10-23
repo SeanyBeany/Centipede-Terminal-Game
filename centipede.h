0f70aeaf69328d8d741c83fbbdac7d6a8b1f056d
SeanyBeany <24shawn@hotmail.ca>
2022-10-23 19:53:59 -0300

    Basic Player Done

--- centipede.h
+++ centipede.h
@@ -10,8 +10,17 @@
 #define MOVE_DOWN 's'
 #define SHOOT ' '
 #define QUIT 'q'
+#define SPACE ' '
+#define CHARACTER_HEIGHT 1
+#define BULLET_HEIGHT 1
 #define PTHREAD_MUTEX_NORMAL 0
+#define BOARD_TOP 13
+#define BOARD_BOTTOM 14
+#define BOARD_LEFT_SIDE 1
+#define BOARD_RIGHT_SIDE 78
+#define BOARD_MIDDLE 40
 void runCentipede();
 void movePlayer();
+void bullet();
 
 #endif
