11f2dcc5ed56f772cf716d5677950e7273a14a12
SeanyBeany <24shawn@hotmail.ca>
2022-10-23 19:53:49 -0300

    Basic Player Done

--- centipede.c
+++ centipede.c
@@ -43,32 +43,36 @@ char *GAME_BOARD[] = {
 
 
 pthread_mutex_t board_mutex; // mutex for board
+pthread_t t1, t2;
+int characterRow;
+int characterCol;
 
 int main(int argc, char**argv) 
 {
-	//int i; // counter for loop iteration
-    pthread_t t1;
     pthread_mutex_init(&board_mutex, NULL);
     if (consoleInit(GAME_ROWS, GAME_COLS, GAME_BOARD));
-    pthread_create(&t1, NULL, (void *) &movePlayer, NULL);
-    sleep(5);
-    pthread_mutex_lock(&board_mutex);
+    if (pthread_create(&t1, NULL, (void *) &movePlayer, NULL) != 0){
+        perror("pthread_create");
+    }
+    sleep(10);
+    
     finalKeypress();
     consoleFinish();
-    pthread_mutex_unlock(&board_mutex);
+    if (pthread_join(t1, NULL) != 0) {
+        perror("pthread_join");
+    }
 }
 
 void movePlayer() {
     fd_set set; // what to check for our select call
-    int CHARACTER_HEIGHT = 1;
     bool gameRunning = true;
-    int row = 5;
-    int col = 30;
-    char* ENEMY_BODY[1][1] = {{"@"}};
-    char** tile = ENEMY_BODY[0];
+    characterRow = BOARD_BOTTOM+1;
+    characterCol = BOARD_MIDDLE;
+    char* CHARACTER[1][1] = {{"@"}};
+    char** characterTile = CHARACTER[0];
 
     pthread_mutex_lock(&board_mutex);
-    consoleDrawImage(row, col, tile, 1);
+    consoleDrawImage(characterRow, characterCol, characterTile, 1);
     consoleRefresh();
     pthread_mutex_unlock(&board_mutex);
 
@@ -81,29 +85,83 @@ void movePlayer() {
             printf("ret = %d\n", ret);
             printf(" timeout\n");
         }
-        while(true){
+        while(gameRunning){
             if(gameRunning && ret >= 1) {
                 char c = getchar();
-                pthread_mutex_lock(&board_mutex);
-                consoleClearImage(row,col, CHARACTER_HEIGHT, strlen(tile[0]));
-                if (c == MOVE_LEFT) {
-                    col-= 1;
-                } else if (c == MOVE_RIGHT) {
-                    col+= 1;
-                } else if (c == MOVE_DOWN) {
-                    row+= 1;
-                } else if (c == MOVE_UP) {
-                    row-= 1;
-                }
-                else if (c == QUIT) {
+
+                if (c == QUIT) {
                     gameRunning = false;
                 }
-                consoleDrawImage(row, col, tile, CHARACTER_HEIGHT);
-                consoleRefresh();
-                pthread_mutex_unlock(&board_mutex);
+                else if (c == SPACE) {
+                    pthread_create(&t2, NULL, (void *) &bullet, NULL);
+                }
+                else {
+                    pthread_mutex_lock(&board_mutex);
+                    consoleClearImage(characterRow,characterCol, CHARACTER_HEIGHT, strlen(characterTile[0]));
+                    if (c == MOVE_LEFT) {
+                        if (characterCol >= BOARD_LEFT_SIDE){
+                            characterCol-= 1;
+                        }
+                    } else if (c == MOVE_RIGHT) {
+                        if (characterCol <= BOARD_RIGHT_SIDE){
+                            characterCol+= 1;
+                        }
+                    } else if (c == MOVE_DOWN) {
+                        if (characterRow <= BOARD_BOTTOM){
+                            characterRow+= 1;
+                        }
+                    } else if (c == MOVE_UP) {
+                        if (characterRow >= BOARD_TOP){
+                            characterRow-= 1;
+                        }
+                    } else if (c == SPACE) {
+                        pthread_create(&t1, NULL, (void *) &bullet, NULL);
+                    }
+                    else if (c == QUIT) {
+                        gameRunning = false;
+                    }
 
+                    consoleDrawImage(characterRow, characterCol, characterTile, CHARACTER_HEIGHT);
+                    consoleRefresh();
+
+                    pthread_mutex_unlock(&board_mutex);
+                }
             }
         }
 
     }
+}
+
+void bullet() {
+    bool gameRunning = true;
+    int row = 15;
+    int col = 30;
+    char* BULLET[1][1] = {{"|"}};
+    char** bulletTile = BULLET[0];
+    int bulletHeight = 0;
+    int hit = 0;
+    int bulletRow = characterRow;
+    int bulletCol = characterCol;
+    
+    while(!hit){
+        sleepTicks(15);
+        if(bulletRow-bulletHeight != 2) {
+            pthread_mutex_lock(&board_mutex);
+            if(bulletHeight >= 1){
+                consoleClearImage(bulletRow-bulletHeight, bulletCol, BULLET_HEIGHT, strlen(bulletTile[0]));
+            }
+            bulletHeight++;
+            consoleDrawImage(bulletRow-bulletHeight, bulletCol, bulletTile, BULLET_HEIGHT);
+            consoleRefresh();
+            pthread_mutex_unlock(&board_mutex);
+        }
+        else {
+            pthread_mutex_lock(&board_mutex);
+            consoleClearImage(bulletRow-bulletHeight, bulletCol, BULLET_HEIGHT, strlen(bulletTile[0]));
+            consoleRefresh();
+            pthread_mutex_unlock(&board_mutex);
+            hit = 1;
+        }
+    }
+    pthread_exit(&t2);
 }
